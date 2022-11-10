879da43ab906b02ded422066ba5c110abece3f6b
Miist3rNyz <kevinkesteloot76@gmail.com>
2022-11-10 11:30:09 +0100

    passage des paramètres username et password en paramètres dans la requête afin de stocker dans la base de données

--- src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
+++ src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
@@ -3,15 +3,17 @@ package com.example.flighttracker.Controller;
 import com.example.flighttracker.Service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UtilisateurController {
     @Autowired
     private UtilisateurService service;
-    @GetMapping("/user")
-    public int test(){
-        service.SaveUtilisateur();
+    @PostMapping("/user")
+    public int test(@RequestParam("username")String username, @RequestParam("password")String password){
+        service.SaveUtilisateur(username,password);
         return 0;
     }
 }
