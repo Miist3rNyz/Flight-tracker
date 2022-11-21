b0cc39f320b62f3377a9a6c72a4367eb059ee11c
Miist3rNyz <kevinkesteloot76@gmail.com>
2022-11-21 10:29:10 +0100

    Liaison entre le front et le back pas encore totalement effective

--- src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
+++ src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
@@ -2,18 +2,21 @@ package com.example.flighttracker.Controller;
 
 import com.example.flighttracker.Model.Utilisateur;
 import com.example.flighttracker.Service.UtilisateurService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.ModelAndView;
+@Data
 @RestController
 public class UtilisateurController {
     @Autowired
     private UtilisateurService service;
-    @PostMapping("/user")
-    public int test(@RequestParam("username")String username, @RequestParam("password")String password){
-        service.SaveUtilisateur(username,password);
 
-        return 0;
+   @PostMapping("/SaveUtilisateur")
+    public ModelAndView SaveUtilisateur(@ModelAttribute Utilisateur utilisateur){
+        service.SaveUtilisateur(utilisateur.getUsername(),utilisateur.getPassword());
+
+        return new ModelAndView("redirect:/");
     }
     @DeleteMapping("/user/{id}")
     public void deleteUser(@PathVariable Long id)
