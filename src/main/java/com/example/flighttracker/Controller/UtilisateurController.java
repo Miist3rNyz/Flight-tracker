2992cc6a47829d312e0d3a4e8ab91546b6bfe90e
Miist3rNyz <kevinkesteloot76@gmail.com>
2022-11-21 14:34:13 +0100

    Inscription fonctionnelle

--- src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
+++ src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
@@ -4,6 +4,7 @@ import com.example.flighttracker.Model.Utilisateur;
 import com.example.flighttracker.Service.UtilisateurService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 @Data
@@ -12,16 +13,21 @@ public class UtilisateurController {
     @Autowired
     private UtilisateurService service;
 
-   @PostMapping("/SaveUtilisateur")
-    public ModelAndView SaveUtilisateur(@ModelAttribute Utilisateur utilisateur){
-        service.SaveUtilisateur(utilisateur.getUsername(),utilisateur.getPassword());
 
-        return new ModelAndView("redirect:/");
-    }
     @DeleteMapping("/user/{id}")
     public void deleteUser(@PathVariable Long id)
     {
         service.deleteById(id);
 
     }
+    @PostMapping("/Login")
+       public int CheckLogin(@ModelAttribute Utilisateur utilisateur) {
+        if (service.CheckLogin(utilisateur.getUsername(), utilisateur.getPassword())==true) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
 }
