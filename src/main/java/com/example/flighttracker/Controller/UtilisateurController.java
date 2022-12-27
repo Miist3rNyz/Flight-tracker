a80dec8d9b5b47526e832dc6def1e53ebdb419d4
Miist3rNyz <kevinkesteloot76@gmail.com>
2022-12-27 22:17:37 +0100

    Ajout d'un profil

--- src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
+++ src/main/java/com/example/flighttracker/Controller/UtilisateurController.java
@@ -4,6 +4,7 @@ import com.example.flighttracker.Service.UtilisateurService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,11 +18,12 @@ public class UtilisateurController {
 
 
     @DeleteMapping("/user/{id}")
-    public void deleteUser(@PathVariable Long id)
-    {
+    public void deleteUser(@PathVariable Long id) {
         service.deleteById(id);
 
     }
+    @GetMapping("/profil")
+    public ModelAndView profil(){return new ModelAndView("redirect:profile");}
     @GetMapping("/home")
     public ModelAndView home(){return new ModelAndView("redirect:/");}
 
